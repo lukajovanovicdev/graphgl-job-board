1980d351699c321b39afd901156c61039cdd2f5d
Luka Jovanovic <luka.jovanovic.1995@gmail.com>
2022-12-15 18:49:00 +0100

    Apply Apollo middleware

--- server/server.js
+++ server/server.js
@@ -5,7 +5,8 @@ import jwt from 'jsonwebtoken';
 import { User } from './db.js';
 
 import { ApolloServer } from 'apollo-server-express';
-import { readFile } from 'fs';
+import { readFile } from 'fs/promises';
+import { resolvers } from './resolvers.js';
 
 const PORT = 9000;
 const JWT_SECRET = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
@@ -34,7 +35,10 @@ app.post('/login', async (req, res) => {
 
 const typeDefs = await readFile('./schema.graphql', 'utf-8');
 const apolloServer = new ApolloServer({ typeDefs, resolvers });
+await apolloServer.start();
+apolloServer.applyMiddleware({ app, path: '/graphql' });
 
 app.listen({ port: PORT }, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log(`GraphQL endpoint: http://localhost:${PORT}/graphql`);
 });
