e05b8c2276943c47639cd5a11f9489c475368160
Luka Jovanovic <luka.jovanovic.1995@gmail.com>
2022-12-16 16:54:12 +0100

    Implement useCreateJob hook

--- client/src/graphql/hooks.js
+++ client/src/graphql/hooks.js
@@ -1,5 +1,6 @@
-import { COMPANY_QUERY, JOBS_QUERY, JOB_QUERY } from '../graphql/queries';
-import { useQuery } from '@apollo/client';
+import { COMPANY_QUERY, CREATE_JOB_MUTATION, JOBS_QUERY, JOB_QUERY } from '../graphql/queries';
+import { useMutation, useQuery } from '@apollo/client';
+import { getAccessToken } from '../auth';
 
 export const useJobs = () => {
   const { data, loading, error } = useQuery(JOBS_QUERY, { fetchPolicy: 'network-only' });
@@ -23,6 +24,32 @@ export const useCompany = (id) => {
   const { data, loading, error } = useQuery(COMPANY_QUERY, { variables: { id } });
   return {
     company: data?.company,
+    loading,
+    error: !!error,
+  };
+};
+
+export const useCreateJob = () => {
+  const [mutate, { loading, error }] = useMutation(CREATE_JOB_MUTATION);
+
+  return {
+    createJob: async (title, description) => {
+      const {
+        data: { job },
+      } = await mutate({
+        variables: { input: { title, description } },
+        context: { headers: { Authorization: 'Bearer ' + getAccessToken() } },
+        update: (cache, { data: { job } }) => {
+          cache.writeQuery({
+            query: JOB_QUERY,
+            variables: { id: job.id },
+            data: { job },
+          });
+        },
+      });
+
+      return job;
+    },
     loading,
     error: !!error,
   };
